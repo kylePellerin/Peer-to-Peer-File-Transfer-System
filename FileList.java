a3a7e3b03fe13d7fb48567548ae77db7de2efc88
kylePellerin <kpellerin@hopper.bowdoin.edu>
2025-11-20 12:40:09 -0500

    fiellist rework.

--- FileList.java
+++ FileList.java
@@ -1,19 +1,13 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 public class FileList {
-    private HashMap<String, Boolean> files; // boolean for has file now we will ahve to change later to comply with partial files
+    private ArrayList<String> files; // boolean for has file now we will ahve to change later to comply with partial files
     // stirng is ip and bool is has file or not 
     public FileList() {
-        files = new HashMap<String, Boolean>();
+        files = new ArrayList<String>(); 
     }
 
     private void addFile(String fileName) {
-        files.put(fileName, true);
-    }
-
-    private void updateFileStatus(String fileName, Boolean status) {
-        if (files.containsKey(fileName)) {
-            files.put(fileName, status);    
-        }
+        files.add(fileName);
     }
 
     private void removeFile(String fileName) {
@@ -21,6 +15,10 @@ public class FileList {
     }
 
     private boolean hasFile(String fileName) {
-        return files.getOrDefault(fileName, false);
+        return files.contains(fileName);
+    }
+
+    private ArrayList<String> getFiles() {
+        return files;
     }
 }
