076efde271c2de2b89fac5642dc7c7ee7d536d90
kylePellerin <kpellerin@hopper.bowdoin.edu>
2025-12-09 12:39:26 -0500

    mati.

--- sync.sh
+++ sync.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
 PRIMARY="54.205.35.150"
 BACKUP="54.226.158.73"
 USER="kpellerin"
 
+FILES="*.java peer.py Makefile lib"
+
 echo "--- Syncing to PRIMARY ($PRIMARY) ---"
-scp -o StrictHostKeyChecking=no *.java $USER@$PRIMARY:~/
+scp -r -o StrictHostKeyChecking=no $FILES $USER@$PRIMARY:~/
 ssh -o StrictHostKeyChecking=no $USER@$PRIMARY "javac -cp '.:lib/*' *.java"
 echo "Done."
 
 echo "--- Syncing to BACKUP ($BACKUP) ---"
-scp -o StrictHostKeyChecking=no *.java $USER@$BACKUP:~/
+scp -r -o StrictHostKeyChecking=no $FILES $USER@$BACKUP:~/
 ssh -o StrictHostKeyChecking=no $USER@$BACKUP "javac -cp '.:lib/*' *.java"
 echo "Done."
 
